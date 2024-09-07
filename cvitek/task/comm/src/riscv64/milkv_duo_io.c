1b5b8de2c35db5929f6361a2d88a192fa2da3382
HenryChaing <n96121210@gs.ncku.edu.tw>
2024-09-07 12:46:30 +0800

    Adapt the LED function for the Duo256M
    The original ThreadX LED function was implemented for the Duo. I updated
    the mapping address to be compatible with the Duo256M, ensuring the LED
    functionality works.

--- cvitek/task/comm/src/riscv64/milkv_duo_io.c
+++ cvitek/task/comm/src/riscv64/milkv_duo_io.c
@@ -1,12 +1,15 @@
 #include "milkv_duo_io.h"
 
+#define GPIO_LED 0x05021000
+#define GPIO_LED_NR 2
+
 void duo_led_control(int enable)
 {
-	*(uint32_t*)(GPIO2 | GPIO_SWPORTA_DDR) = 1 << 24;
+	*(uint32_t*)(GPIO_LED | GPIO_SWPORTA_DDR) = 1 << GPIO_LED_NR;
 
 	if (enable) {
-		*(uint32_t*)(GPIO2 | GPIO_SWPORTA_DR) = 1 << 24;
+		*(uint32_t*)(GPIO_LED | GPIO_SWPORTA_DR) = 1 << GPIO_LED_NR;
 	} else {
-		*(uint32_t*)(GPIO2 | GPIO_SWPORTA_DR) = 0;
+		*(uint32_t*)(GPIO_LED | GPIO_SWPORTA_DR) = 0;
 	}
 }
